ef86e4763308bc5c3d53aa425f6c2a26ae980bd8
Candice Mathison <candicemathison@Candices-MacBook-Air.local>
2025-05-27 16:50:22 -0600

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of a favorite pet?\n")
+
+print("You're band name could be " + city + " " +  pet + "!")
