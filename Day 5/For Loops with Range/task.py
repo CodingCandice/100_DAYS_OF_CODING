1111265e706bf2fac30fb5fb84eb4e7a6eb7f22d
candicemathison <candicemathison@yahoo.ca>
2025-06-04 09:57:31 -0600

    Learned loops and range

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,10 @@
+# Range Function with For Loop
+
+for number in range(1, 11, 3): #3 being the step size.)
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+    print(total)
+
