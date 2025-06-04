1317356713c3342f291d4fea7eb927ef881c0281
candicemathison <candicemathison@yahoo.ca>
2025-06-04 08:56:36 -0600

    Rock, Paper Scissors success!

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,8 +1,3 @@
-print("Welcome to the Band Name Generator!")
 
-city = input("What city did you grow up in?\n")
-
-pet = input("What is the name of a favorite pet?\n")
-
-print("You're band name could be " + city + " " +  pet + "!")
+print ("Hello world!")
 
--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,11 @@
+
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_friend = random.choice(friends)
+
+print(f"Your random friend is: {random_friend}")
+
+print(random.choice(friends))
+
